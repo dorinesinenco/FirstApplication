7027520d6e1d91271b0ec26e6beb4e842e0d0521
dorinesinenco <dorin.e.art@gmail.com>
2020-04-07 21:23:31 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,8 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    System.out.println("Here is another LINE!");
   }
 }
 
-// command prompt
-// cli
-// terminal
-
-// OS -> environment variables
-// JAVA_HOME, PATH
-
-// javac MyJavaClass.java 
-// java  MyJavaClass.class
-
-//"javac" не является внутренней или внешней
-//командой, исполняемой программой или пакетным файлом.
 
